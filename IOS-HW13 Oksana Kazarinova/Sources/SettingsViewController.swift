fd1f04b94dad822baa14f81f89f9ad745e7fa3e8
okcreating <oksibearus@gmail.com>
2024-05-20 12:17:37 +0100

    Project adjustments

--- IOS-HW13 Oksana Kazarinova/Sources/SettingsViewController.swift
+++ IOS-HW13 Oksana Kazarinova/Sources/SettingsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SettingsViewController.swift
 //  IOS-HW13 Oksana Kazarinova
 //
 //  Created by Oksana Kazarinova on 20/05/2024.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGray5
+        
     }
+    
 
 
 }
-
